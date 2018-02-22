f9bf8c8a2be93a856606b0f1d6268ff0c5100472
marb61a <marb61a@gmail.com>
2018-02-22 17:22:39 +0000

    added button and onpress

--- App.js
+++ App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
   state = {
-    placeName: ''
+    placeName: '',
+    places: []
   }
 
-  placeNameChangedHandler = event => {
-    alert(event);
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+  placeSubmitHandler = () => {
+    if(this.state.placeName.trim() === ""){
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      };
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <TextInput 
-          style={{width: 300}}
-          placeholder="A beautiful place"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangedHandler}
-        />
+        <View style={styles.inputContainer}>
+          <TextInput 
+            style={{width: 300}}
+            placeholder="A beautiful place"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+          />
+          <Button 
+            title="add" 
+            style={styles.placeButton} 
+            onPress={this.placeSubmitHandler}
+          />
+        </View>
       </View>
     );
   }
